2fbdc89d20260d685efce2b150230f8ccce06517
xandymelo <xandymelo@outlook.com.br>
2025-11-14 20:25:12 -0300

    fix: remove valid file

--- daily/src/main/java/com/project/daily/controller/AuthController.java
+++ daily/src/main/java/com/project/daily/controller/AuthController.java
@@ -2,16 +2,11 @@ package com.project.daily.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
 
 import com.project.daily.model.request.LoginRequest;
 import com.project.daily.model.request.RegisterRequest;
-import com.project.daily.model.response.JwtResponse;
 import com.project.daily.services.AuthService;
-import com.project.daily.utils.JwtUtil;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -26,7 +21,7 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody @Valid LoginRequest login) {
+    public ResponseEntity<?> login(@RequestBody LoginRequest login) {
         return authService.login(login);
     }
 
