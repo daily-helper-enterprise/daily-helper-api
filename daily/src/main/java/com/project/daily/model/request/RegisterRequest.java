210b1e3a451fd893cffdaf5de262a4728db98951
Artur Guimarães <51428160+Guimaaaas@users.noreply.github.com>
2025-11-20 13:22:49 -0300

    Codigo de Member

--- daily/src/main/java/com/project/daily/model/request/RegisterRequest.java
+++ daily/src/main/java/com/project/daily/model/request/RegisterRequest.java
@@ -8,4 +8,8 @@ public class RegisterRequest {
     private String email;
     private String username;
     private String password;
+    public Long getRoleId() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRoleId'");
+    }
 }
